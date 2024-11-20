ced7223cf2075e68ebafb045ac474a3889348265
Si11ycheese <si11ycheese6680@gmail.com>
2024-11-20 08:56:31 +0800

    update

--- build_files.sh
+++ build_files.sh
@@ -4,12 +4,25 @@
 
 
 # build_files.sh
-set -e  # 如果任何命令失败，则退出脚本
+# set -e  # 如果任何命令失败，则退出脚本
 
-echo "Installing dependencies..."
+# echo "Installing dependencies..."
+# python3.9 -m pip install -r requirements.txt
+
+# echo "Collecting static files..."
+# python3.9 manage.py collectstatic --noinput
+
+# echo "Build completed successfully."
+
+
+
+#Build the project
+echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
-echo "Collecting static files..."
-python3.9 manage.py collectstatic --noinput
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
-echo "Build completed successfully."
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
